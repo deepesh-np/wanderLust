370d0eda1aa86273dd4a6d99e30425809354152c
deepesh-np <nrndpsh@gmail.com>
2025-07-12 22:08:38 +0530

    downscaled image in edit using cloudfare imafe prop

--- controllers/listing.js
+++ controllers/listing.js
@@ -52,9 +52,10 @@ module.exports.editListing = async (req, res) => {
   if (!listing) {
     req.flash('error', 'Listing you requested for does not exist');
     res.redirect('/listings');
-  } else {
-    res.render('listings/edit.ejs', { listing });
   }
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace('/upload', '/upload/w_250');
+  res.render('listings/edit.ejs', { listing, originalImageUrl });
 };
 
 //Update Route
@@ -62,7 +63,7 @@ module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-  if (typeof req.file !== "undefined") {
+  if (typeof req.file !== 'undefined') {
     let url = req.file.path;
     let filename = req.file.filename;
     listing.image = { url, filename };
