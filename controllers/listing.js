9aa67af63463c1c0425a881e0d67283d759b39bb
deepesh-np <nrndpsh@gmail.com>
2025-07-14 00:26:39 +0530

    gst added

--- controllers/listing.js
+++ controllers/listing.js
@@ -48,11 +48,17 @@ module.exports.show = async (req, res) => {
 
 //Create Route
 module.exports.createNewListing = async (req, res, next) => {
+  // const maptilerclient = await import('@maptiler/client');
+  // maptilerclient.config.apiKey = process.env.MAPTILER_API_KEY;
+  // const result = await maptilerclient.geocoding.forward('berlin');
+  // console.log(result.features[0].geometry);
+
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
+
   await newListing.save();
   req.flash('success', 'New listing created');
   res.redirect('/listings');
