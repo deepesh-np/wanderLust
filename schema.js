6e3b82dbdd55186bf56b110dd3c9ace717b7e2c4
deepesh-np <nrndpsh@gmail.com>
2025-07-13 11:56:39 +0530

    new, edit, show listing updated to modify category

--- schema.js
+++ schema.js
@@ -10,10 +10,21 @@ module.exports.listingSchema = Joi.object({
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
     image: Joi.string().allow('', null),
+    category: Joi.string()
+      .valid(
+        'mountain',
+        'arctic',
+        'farm',
+        'desert',
+        'iconic cities',
+        'camping',
+        'surfing'
+      )
+      .required(),
   }).required(),
 });
 
-module.exports.reviewSchema = Joi.object ({
+module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().required().min(1).max(5),
     Comment: Joi.string().required(),
